c895e7a036c0d4ada5138db256fc55aa699a46d5
nicola.defiorenze <nicola.defiorenze@ennova-research.com>
2016-03-25 17:04:39 +0100

    initial import

---
+++ app/src/main/java/com/ndefiorenze/blursample/MainActivity.java
@@ -0,0 +1,107 @@
+package com.ndefiorenze.blursample;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.ndefiorenze.blur.BlurEngine;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int ANIMATION_DURATION = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewGroup blurView = (ViewGroup) findViewById(R.id.blur_view);
+        final View blueBox = findViewById(R.id.blue_box);
+        final View redBox = findViewById(R.id.red_box);
+
+        ImageView blurDestinationView = (ImageView) findViewById(R.id.blur_content);
+
+        initAnimations(blurView, blueBox, redBox);
+
+
+        final BlurEngine blurEngine = BlurEngine.getInstance(this);
+        blurEngine.blur(blurView, blurDestinationView);
+
+
+        Button blurButton = (Button) findViewById(R.id.start_blur_button);
+        blurButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!blurEngine.isRunning()) {
+                    blurEngine.start(BlurEngine.Repeat.YES);
+                } else {
+                    blurEngine.stop();
+                }
+            }
+        });
+    }
+
+    private void initAnimations(final ViewGroup blurView, final View blueBox, final View redBox) {
+        blurView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                final float topY = blueBox.getY();
+                final float bottomY = redBox.getY();
+
+                final Handler handlerBlueBox = new Handler();
+                handlerBlueBox.post(new Runnable() {
+                    private Runnable runnable;
+                    float bottom = bottomY;
+                    float top = 0;
+                    boolean up = true;
+
+                    @Override
+                    public void run() {
+                        runnable = this;
+
+                        blueBox.animate().translationY(up ? top : bottom).rotation(up ? 90 : -90).setDuration(ANIMATION_DURATION).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                up = !up;
+                                handlerBlueBox.post(runnable);
+                            }
+                        });
+                    }
+                });
+
+                final Handler handlerRedBox = new Handler();
+                handlerRedBox.post(new Runnable() {
+                    private Runnable runnable;
+                    float bottom = 0;
+                    float top = -bottomY + topY;
+                    boolean up = false;
+
+                    @Override
+                    public void run() {
+                        runnable = this;
+
+                        redBox.animate().translationY(up ? top : bottom).scaleX(up ? 2 : -2).scaleY(up ? -2 : 2).setDuration(ANIMATION_DURATION).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                up = !up;
+                                handlerRedBox.post(runnable);
+                            }
+                        });
+                    }
+                });
+                blurView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
+    }
+
+
+}
---
+++ blur/src/main/java/com/ndefiorenze/blur/BlurEngine.java
@@ -0,0 +1,124 @@
+package com.ndefiorenze.blur;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * created on: 2016-03-24
+ *
+ * @author Nicola De Fiorenze
+ */
+public class BlurEngine {
+
+    public static final int RADIUS = 12;
+    public static final int SCALE_FACTOR = 8;
+    private static final String TAG = BlurEngine.class.getName();
+    private static BlurEngine instance = null;
+    private final Context context;
+    private final Handler handler = new Handler();
+    long sum = 0;
+    long count = 0;
+    private StatusListener statusListener;
+    private boolean run = false;
+    private Runnable runnableCode;
+    private Repeat repeat;
+
+    private ImageView destination;
+
+    private BlurEngine(Context context) {
+        this.context = context;
+    }
+
+    public static BlurEngine getInstance(Context context) {
+        if (instance == null) {
+            instance = new BlurEngine(context);
+        }
+        return instance;
+    }
+
+    public void setStatusListener(StatusListener statusListener) {
+        this.statusListener = statusListener;
+    }
+
+    private void blurView(View source, ImageView destination) {
+        long startMs = System.currentTimeMillis();
+        long totalTime = 0;
+        if (run) {
+            float scaleFactor = SCALE_FACTOR;
+            float radius = RADIUS;
+            try {
+                Bitmap overlay = Bitmap.createBitmap((int) (source.getMeasuredWidth() / scaleFactor),
+                        (int) (source.getMeasuredHeight() / scaleFactor), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(overlay);
+                canvas.translate(-source.getLeft() / scaleFactor, -source.getTop() / scaleFactor);
+                canvas.scale(1 / scaleFactor, 1 / scaleFactor);
+                Paint paint = new Paint();
+                paint.setFlags(Paint.FILTER_BITMAP_FLAG);
+                source.draw(canvas);
+                overlay = FastBlur.doBlur(overlay, (int) radius, true);
+                destination.setImageDrawable(new BitmapDrawable(context.getResources(), overlay));
+                totalTime = System.currentTimeMillis() - startMs;
+                Log.d(TAG, "Time for blur(): " + totalTime + "ms");
+            } catch (Exception e) {
+                Log.e(TAG, "blur: ", e);
+            }
+            if (statusListener != null)
+                statusListener.finish(totalTime);
+
+            sum += totalTime;
+            count++;
+            if (sum < 0) {
+                sum = 0;
+                count = 1;
+            }
+
+            if (repeat == Repeat.YES)
+                handler.postDelayed(runnableCode, 2 * (sum / count));
+            Log.d(TAG, "finish() called with: " + "milliseconds = [" + totalTime + "] med= [" + (sum / count) + "]");
+        }
+    }
+
+    public void blur(final View source, final ImageView destination) {
+        this.destination = destination;
+        runnableCode = new Runnable() {
+            @Override
+            public void run() {
+                blurView(source, destination);
+            }
+        };
+    }
+
+    public void stop() {
+        run = false;
+        destination.setImageResource(android.R.color.transparent);
+    }
+
+    public void start() {
+        start(Repeat.YES);
+    }
+
+    public void start(Repeat repeat) {
+        if (!run) {
+            this.repeat = repeat;
+            run = true;
+            handler.post(runnableCode);
+        }
+    }
+
+    public boolean isRunning(){
+        return run;
+    }
+
+    public enum Repeat {YES, NO}
+
+    public interface StatusListener {
+        void finish(long milliseconds);
+    }
+}
